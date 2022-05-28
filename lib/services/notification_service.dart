9ef76eb3155798f32579b82a669ede1653cb6f8d
Emeka Jideije <jideije.emeka@gmail.com>
2022-05-29 00:24:35 +0100

    Add the display notification function for both OS

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -24,7 +24,46 @@ class NotificationService{
     );
     await notificationsPlugin.initialize(
     initializationSettings,
+    onSelectNotification: selectNotification);
+  }
+
+  void requestIOSPermissions() {
+    notificationsPlugin.resolvePlatformSpecificImplementation
+    <IOSFlutterLocalNotificationsPlugin>()?.requestPermissions(
+      alert: true,
+      sound: true,
+      badge: true,
     );
+  }
+
+  displayNotification({required String title, required String body}) async {
+    debugPrint("doing test");
+
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        "channelId", "channelName",
+        importance: Importance.max,
+        priority: Priority.high,
+        icon: "cloud_icon");
+
+    var iosPlatformChannelSpecifics = const IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iosPlatformChannelSpecifics,
+    );
+
+    await notificationsPlugin.show(
+        0,
+        title, body,
+        platformChannelSpecifics, payload: "Default_Sound");
+  }
+  
+  Future selectNotification(String? payLoad) async{
+    if(payLoad != null) {
+      debugPrint("Notification payload: $payLoad");
+    }else{
+      debugPrint("Notification Done");
+    }
+    Get.to(() => Container(color: Colors.white,));
   }
 
   Future onDidReceiveLocalNotification(int id,
