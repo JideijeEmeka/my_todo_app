b7875853636abec76f7eb74d3d97b2ef95a08a3c
Emeka Jideije <jideije.emeka@gmail.com>
2022-06-24 15:03:16 +0100

    Edit the app theme

--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const Color bluishColor = Color(0xFF4e5ae8);
 const Color yellowishColor = Color(0xFFFFb746);
 const Color pinkishColor = Color(0xFFff4667);
-const Color white = Colors.white;
+const Color whiteColor = Colors.white;
 const Color primaryColor = bluishColor;
 const Color darkGreyColor = Color(0xFF121212);
 const Color darkHeaderColor = Color(0xFF424242);
