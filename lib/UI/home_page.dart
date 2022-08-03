8df250568a0aa835419c5bbad2a86a6de919095b
Emeka Jideije <jideije.emeka@gmail.com>
2022-08-04 00:24:37 +0100

    Update home page with custom widgets

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,6 +1,12 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:my_todo_app/services/notification_service.dart';
+import 'package:my_todo_app/themes/app_colors.dart';
+import 'package:my_todo_app/widgets/date_picker_widget.dart';
 import 'package:my_todo_app/widgets/home_page_app_bar.dart';
+import 'package:my_todo_app/widgets/task_bar_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -23,14 +29,13 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      backgroundColor: context.theme.backgroundColor,
       appBar: homePageAppBar(context),
       body: Column(children: [
-        Row(children: [
-          Column(children: [
-
-          ],)
-        ],)
+        taskBar(),
+        datePicker(),
       ],),
     );
   }
