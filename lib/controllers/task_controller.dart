fae440c63259045903c8c6705eaa3d61de9fd455
Emeka Jideije <jideije.emeka@gmail.com>
2022-08-05 18:17:56 +0100

    Add a controller for whole task

---
+++ lib/controllers/task_controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:my_todo_app/models/task.dart';
+
+class TaskController extends GetxController {
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  Future<void> addTask({Task? task}) {
+    return null;
+  }
+}
