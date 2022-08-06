dd5770e50f06f58090992f3bf8c2e93fab3c178c
Emeka Jideije <jideije.emeka@gmail.com>
2022-08-06 14:01:03 +0100

    Add more textstyles to app widgets

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app_colors.dart';
 
@@ -10,4 +12,44 @@ class Themes{
   static final darkTheme = ThemeData(
       brightness: Brightness.dark,
       backgroundColor: darkGreyColor,);
+}
+
+TextStyle get subHeadingStyle {
+  return GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      color: Get.isDarkMode ? Colors.grey[400] : Colors.grey
+    )
+  );
+}
+
+TextStyle get headingStyle {
+  return GoogleFonts.lato(
+      textStyle: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Get.isDarkMode ? whiteColor : blackColor
+      )
+  );
+}
+
+TextStyle get titleStyle {
+  return GoogleFonts.lato(
+      textStyle: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: Get.isDarkMode ? whiteColor : blackColor
+      )
+  );
+}
+
+TextStyle get subTitleStyle {
+  return GoogleFonts.lato(
+      textStyle: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: Get.isDarkMode ? Colors.grey[100] : Colors.grey[600]
+      )
+  );
 }
