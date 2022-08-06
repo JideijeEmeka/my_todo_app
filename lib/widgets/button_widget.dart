53908af2ee0b8feb9a1a10a01aa6df07e01bb9ce
Emeka Jideije <jideije.emeka@gmail.com>
2022-08-06 13:54:27 +0100

    Build a custom button

--- lib/widgets/button_widget.dart
+++ lib/widgets/button_widget.dart
@@ -11,8 +11,9 @@ class MyButton extends StatelessWidget {
     return ElevatedButton(onPressed: onPressed,
         style: ElevatedButton.styleFrom(
           primary: primaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.circular(15),
           ),
         ),
         child: Text(label, style: const TextStyle(color: Colors.white),));
