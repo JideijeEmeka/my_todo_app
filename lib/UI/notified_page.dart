b3b7d4cf9476f8ee9e82221ffad56f415f07c804
Emeka Jideije <jideije.emeka@gmail.com>
2022-08-22 22:06:15 +0100

    Design notified page

--- lib/UI/notified_page.dart
+++ lib/UI/notified_page.dart
@@ -29,91 +29,112 @@ class NotifiedPage extends StatelessWidget {
           fontSize: 20
         )),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 30, bottom: 1),
-            child: Text('Hello, Emmy', style: GoogleFonts.lato(
-                textStyle: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Get.isDarkMode ? whiteColor : blackColor
-                )
-            ),),
-          ),
-          Text('You have a new reminder', style: subHeadingStyle,),
-          Container(
-            margin: const EdgeInsets.only(left: 30, right: 30, top: 50),
-              width: MediaQuery.of(context).size.width,
-              height: 400,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: bluishColor,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ///Title
-                    Row(
-                      children: [
-                        const FaIcon(FontAwesomeIcons.bookOpen, size: 35, color: whiteColor,),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text('Title', style: GoogleFonts.lato(textStyle:
-                            const TextStyle(color: whiteColor, fontSize: 26)),),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 25),
-                      child: Text(label.toString().split("|")[0],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 30, bottom: 1),
+              child: Text('Hello, Emmy', style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Get.isDarkMode ? whiteColor : blackColor
+                  )
+              ),),
+            ),
+            Text('You have a new reminder', style: subHeadingStyle,),
+            Container(
+              margin: const EdgeInsets.only(left: 30,
+                  right: 30, top: 30, bottom: 50),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: bluishColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      /// Title
+                      Row(
+                        children: [
+                          const FaIcon(FontAwesomeIcons.bookOpen,
+                            size: 35, color: whiteColor,),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text('Title', style: GoogleFonts.lato(textStyle:
+                              const TextStyle(color: whiteColor, fontSize: 20)),),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25),
+                        child: Text(label.toString().split("|")[0],
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: whiteColor.withOpacity(0.7),),),
+                      ),
+                      /// Description
+                      Row(
+                        children: [
+                          const FaIcon(FontAwesomeIcons.listCheck, size: 35, color: whiteColor,),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text('Description', style: GoogleFonts.lato(textStyle:
+                            const TextStyle(color: whiteColor, fontSize: 20)),),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25),
+                        child: Text(label.toString().split("|")[0],
                           style: TextStyle(
-                            fontSize: 22,
+                            fontSize: 17,
                             color: whiteColor.withOpacity(0.7),),),
-                    ),
-                    ///Description
-                    Row(
-                      children: [
-                        const FaIcon(FontAwesomeIcons.listCheck, size: 35, color: whiteColor,),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text('Description', style: GoogleFonts.lato(textStyle:
-                          const TextStyle(color: whiteColor, fontSize: 26)),),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 25),
-                      child: Text(label.toString().split("|")[0],
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: whiteColor.withOpacity(0.7),),),
-                    ),
-                    ///Date
-                    Row(
-                      children: [
-                        const FaIcon(FontAwesomeIcons.calendarCheck, size: 35, color: whiteColor,),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text('Date', style: GoogleFonts.lato(textStyle:
-                          const TextStyle(color: whiteColor, fontSize: 26)),),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 25),
-                      child: Text(label.toString().split("|")[0],
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: whiteColor.withOpacity(0.7),),),
-                    ),
-                  ],
+                      ),
+                      /// Date
+                      Row(
+                        children: [
+                          const FaIcon(FontAwesomeIcons.calendarCheck, size: 35, color: whiteColor,),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text('Date', style: GoogleFonts.lato(textStyle:
+                            const TextStyle(color: whiteColor, fontSize: 20)),),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25),
+                        child: Text(label.toString().split("|")[0],
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: whiteColor.withOpacity(0.7),),),
+                      ),
+                      /// Time
+                      Row(
+                        children: [
+                          const FaIcon(FontAwesomeIcons.clock, size: 35, color: whiteColor,),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text('Time', style: GoogleFonts.lato(textStyle:
+                            const TextStyle(color: whiteColor, fontSize: 20)),),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25),
+                        child: Text(label.toString().split("|")[0],
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: whiteColor.withOpacity(0.7),),),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
