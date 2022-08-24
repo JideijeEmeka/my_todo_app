7a66b9b0697113e9ee927279bbd512d032675fab
Emeka Jideije <jideije.emeka@gmail.com>
2022-08-24 02:27:30 +0100

    Add profile pic at app bar

--- lib/widgets/home_page_app_bar.dart
+++ lib/widgets/home_page_app_bar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:my_todo_app/UI/notified_page.dart';
+import 'package:my_todo_app/constants/constants.dart';
 import 'package:my_todo_app/main.dart';
-import 'package:my_todo_app/services/notification_service.dart';
+import 'package:my_todo_app/services/notification_service1.dart';
 import 'package:my_todo_app/themes/app_colors.dart';
 
 NotificationService notificationService = NotificationService();
@@ -19,16 +21,25 @@ PreferredSizeWidget homePageAppBar(BuildContext context) {
             body: Get.isDarkMode ? "Activated light theme" : "Activated dark theme"),
         // notificationService.scheduledNotification()
       },
-      child: Icon(Get.isDarkMode ? Icons.wb_sunny
-          : Icons.nightlight_round, size: 23,
-        color: Get.isDarkMode ? Colors.white: Colors.black,),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, top: 22),
+        child: FaIcon(Get.isDarkMode ? FontAwesomeIcons.boltLightning
+            : FontAwesomeIcons.powerOff, size: 25,
+          color: Get.isDarkMode ? Colors.white: Colors.black,),
+      ),
     ),
     actions: [
       Padding(
-        padding: const EdgeInsets.only(right: 15, top: 17),
-        child: FaIcon(FontAwesomeIcons.user, size: 23,
-        color: Get.isDarkMode ? whiteColor : blackColor,),
-      )
+        padding: const EdgeInsets.only(right: 20, top: 15),
+        child: InkWell(
+          onTap: () => Get.to(const NotifiedPage(label: 'label')),
+          child: const CircleAvatar(
+            radius: 19,
+            backgroundColor: bluishColor,
+            backgroundImage: NetworkImage(profilePicUrl),
+          ),
+        ),
+      ),
     ],
   );
 }
